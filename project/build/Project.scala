5250212f79d0f3d040750fbe8eb0e1e83f15a97a
Filippo De Luca <dl.filippo@filosganga.it>
2011-05-13 14:07:23 +0200

    Adds maven publishing feature

--- project/build/Project.scala
+++ project/build/Project.scala
@@ -20,11 +20,21 @@
  */
 
 import sbt._
+import java.net.URL
+import java.util.jar.Attributes.Name._
 
 class SwurflProject(info: ProjectInfo) extends DefaultProject(info) {
 
-  override def managedStyle = ManagedStyle.Maven
-  val publishTo = "Sonatype OSS Maven Repository Snapshots" at "https://oss.sonatype.org/content/repositories/snapshots/"
+
+//  lazy val projectUrl                 = propertyOptional[URL](new URL("http://github.com/ff-dev/swurfl"), true)
+//  lazy val projectInceptionyear       = propertyOptional[Int](2011, true)
+//  lazy val projectOrganizationName    = propertyOptional[String](organization, true)
+//  lazy val projectOrganizationUrl     = propertyOptional[URL](new URL("http://ffdev.org"), true)
+//
+//  lazy val projectLicenseName         = propertyOptional[String]("GPLv3", true)
+//  lazy val projectLicenseUrl          = propertyOptional[URL](new URL("http://www.gnu.org/licenses/gpl-3.0.txt"), true)
+//  lazy val projectLicenseDistribution = propertyOptional[String]("repo", true)
+
 
   override def packageSrcJar= defaultJarPath("-sources.jar")
   val sourceArtifact = Artifact.sources(artifactID)
@@ -34,14 +44,28 @@ class SwurflProject(info: ProjectInfo) extends DefaultProject(info) {
 
   override def packageToPublishActions = super.packageToPublishActions ++ Seq(packageSrc, packageDocs)
 
-  override def pomExtra =
-  <licenses>
-    <license>
-      <name>GPLv3</name>
-      <url>http://www.gnu.org/licenses/gpl-3.0.txt</url>
-      <distribution>repo</distribution>
-    </license>
-  </licenses>
+
+  override def managedStyle = ManagedStyle.Maven
+  // Set up publish repository (the tuple avoids SBT's ReflectiveRepositories detection)
+  private lazy val snapshotPublishRepo = ("Sonatype OSS Maven Repository Snapshots" -> "https://oss.sonatype.org/content/repositories/snapshots/")
+  private lazy val releasePublishRepo  = ("Sonatype OSS Maven Repository Releases"  -> "https://oss.sonatype.org/content/repositories/maven2/")
+
+  val publishTo =
+    if (version.toString.endsWith("-SNAPSHOT")) snapshotPublishRepo._1 at snapshotPublishRepo._2
+    else releasePublishRepo._1 at releasePublishRepo._2
+
+  Credentials(Path.userHome / ".ivy2" / ".credentials", log)
+
+
+
+//  override def pomExtra =
+//  <licenses>
+//    <license>
+//      <name>GPLv3</name>
+//      <url>http://www.gnu.org/licenses/gpl-3.0.txt</url>
+//      <distribution>repo</distribution>
+//    </license>
+//  </licenses>
 
 
 
